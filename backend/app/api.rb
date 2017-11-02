94a50715488b055463f0714124df27ac3be731d5
Raphael Quintanilha dos Santos <raphael_santos@id.uff.br>
2017-11-01 22:48:21 -0200

    Initial commit

---
+++ backend/app/api.rb
@@ -0,0 +1,16 @@
+module Challenge
+  class API < ::Grape::API
+    format :json
+    prefix :api
+
+    include Grape::OAuth2.api
+
+    desc 'Root action'
+
+    get '/' do
+      { error: ['Please check API documentation'] }
+    end
+
+    mount ::Challenge::V1::Base
+  end
+end
---
+++ backend/app/v1/base.rb
@@ -0,0 +1,15 @@
+module Challenge
+  module V1
+    class Base < ::Grape::API
+      version 'v1', using: :path
+
+      desc 'Information about current resource owner'
+
+      get '/me' do
+        access_token_required!
+
+        present(current_resource_owner, with: Entities::User)
+      end
+    end
+  end
+end
